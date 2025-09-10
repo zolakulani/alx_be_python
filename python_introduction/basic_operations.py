d80a1638ac9ea7417a42569c7e9eeb01a319d727
zolakulani <shikwenizolakulani@gmail.com>
2025-09-10 11:13:33 +0200

    basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
