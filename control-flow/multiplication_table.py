8a22f1e2e08cb20c6d1185186e98e77522f3d4c5
zolakulani <shikwenizolakulani@gmail.com>
2025-09-21 16:31:49 +0200

    /tmp/correction/3542089913971922278818274831635994997438_5/100740/480238/control-flow/multiplication_table.py doesn't contain print\s*\(\s*f?['\"]\s*\{\s*number\s*\}\s*\*\s*\{(\w+)\}

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,6 @@
 number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1, 11):
-    product = number * i
-    print(f"{number} x {i} = {product}")
+    X = number
+    Y = i
+    Z = X * Y
+    print(f"{X} x {Y} = {Z}")
