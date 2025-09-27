c8608b536c9c4ba29fd085fe4ac97cc55e86a781
zolakulani <shikwenizolakulani@gmail.com>
2025-09-27 11:29:47 +0200

    switching to if else statement due to checker failing match case statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,24 +1,48 @@
 def perform_operation(num1, num2, operation):   
-    
-    match operation:
-        case "add":
-            result = num1 + num2
-            return result
-                  
-        case "subtract":
-            result = num1 - num2
-            return result
-            
-        case "multiply":
-            result = num1 * num2
-            return result
-            
-        case "divide":
-            if num2 == 0:
-                return f"Cannot divide by zero."
-            else:
-                result = num1 / num2
-                return result
+
+    if "add" == operation:
+        result = num1 + num2
+        return result
                 
-        case _:
-            return f"Unknown operation"
+    elif "subtract" == operation:
+        result = num1 - num2
+        return result
+        
+    elif "multiply" == operation:
+        result = num1 * num2
+        return result
+        
+    elif "divide" == operation:
+        if num2 == 0:
+            return f"Cannot divide by zero."
+        else:
+            result = num1 / num2
+            return result
+                
+    else:
+        return f"Unknown operation"
+    
+    # match operation:
+    #     case "add":
+    #         result = num1 + num2
+    #         return result
+                  
+    #     case "subtract":
+    #         result = num1 - num2
+    #         return result
+            
+    #     case "multiply":
+    #         result = num1 * num2
+    #         return result
+            
+    #     case "divide":
+    #         if num2 == 0:
+    #             return f"Cannot divide by zero."
+    #         else:
+    #             result = num1 / num2
+    #             return result
+                
+    #     case _:
+    #         return f"Unknown operation"
+     
+            
