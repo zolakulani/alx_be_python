c5d9df3433991c92d37e8c97678fcc66dc459f23
zolakulani <shikwenizolakulani@gmail.com>
2025-09-27 13:03:22 +0200

    Checks for implementation of Choice Input as a number

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
 
         if choice == 1:
             # Prompt for and add an item
-           item = input("Enter the item to add: ")
+           item = input("Enter the item add: ")
            shopping_list.append(item)
            print(f"'{item}' added to the shopping list.")
         elif choice == 2:
